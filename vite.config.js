0886c3e4104430af0f3f81ddd9a587a5325c37a7
palomacalvin <calvinpaloma@gmail.com>
2025-07-08 15:46:19 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/clcf_2025/',
   plugins: [react()],
   test: {
     globals: true,
